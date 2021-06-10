935d39e64a4f9b2dbd05c420ece5ad8c9a3c8437
jet.chen <chen_kaikai777@126.com>
2021-06-10 09:49:19 +0800

    690 员工的重要性

--- src/main/java/cn/jet/leetcode/leetcodeFolder/206.反转链表.java
+++ src/main/java/cn/jet/leetcode/leetcodeFolder/206.反转链表.java
@@ -16,6 +16,7 @@
  * }
  */
 class Solution {
+    // 思路：先拿到head，然后往后遍历，同时将前一个node置为后一个node的next
     public ListNode reverseList(ListNode head) {
         if (head == null) return null;
         ListNode result = new ListNode(head.val);
