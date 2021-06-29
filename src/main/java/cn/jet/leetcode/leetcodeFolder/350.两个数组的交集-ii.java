45c7f49cac1c2115d24576639624e712c746614a
jet.chen <chen_kaikai777@126.com>
2021-06-29 10:23:57 +0800

    350 两个数组的交集

---
+++ src/main/java/cn/jet/leetcode/leetcodeFolder/350.两个数组的交集-ii.java
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=350 lang=java
+ *
+ * [350] 两个数组的交集 II
+ */
+
+// @lc code=start
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        List<Integer> list = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : nums1) {
+            if (map.containsKey(i)) {
+                map.put(i, map.get(i)+1);
+            } else {
+                map.put(i, 1);
+            }
+        }
+        for (int i : nums2) {
+            Integer v = map.get(i);
+            if (v != null) {
+                list.add(i);
+                if (v == 1) {
+                    map.remove(i);
+                } else {
+                    map.put(i, v-1);
+                }
+            }
+        }
+        int[] ints = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            ints[i] = list.get(i);
+        }
+        return ints;
+    }
+}
+// @lc code=end
+
